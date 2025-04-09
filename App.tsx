ef35f35c12f445cd7e3b63a567af00e9804aad24
berna-ikiz <brn.ikiz.35@gmail.com>
2025-04-09 18:20:36 +0300

    Emoji picker has been added with bottom sheet  and category add screen has been prepared

--- App.tsx
+++ App.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import StackNavigator from "../ExpenseTracker/src/navigation/StackNavigator";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={styles.container}>
-      <StackNavigator />
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.bottomSheetContainer}>
+      <SafeAreaView style={styles.container}>
+        <StackNavigator />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  bottomSheetContainer: {
+    flex: 1,
+    backgroundColor: "grey",
   },
 });
 
